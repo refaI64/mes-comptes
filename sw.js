0362e69e8984d6aea97bad26eef91a43f20e41a3
refaI64 <refa3ihassan@gmail.com>
2026-04-22 15:40:19 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'mes-comptes-v1';
+const FILES = [
+  '/mes-comptes/',
+  '/mes-comptes/index.html',
+  '/mes-comptes/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
